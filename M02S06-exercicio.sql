f97a556cccd73dec08b2e2704e311c9444d947d4
Pedro Alencar <pedromariutti@gmail.com>
2023-06-09 13:29:26 -0300

    Ex 04 - Criação da tabela de imóvel

--- M02S06-exercicio.sql
+++ M02S06-exercicio.sql
@@ -6,6 +6,23 @@ CREATE TABLE proprietario (
 	nome VARCHAR(20),
 	sobrenome VARCHAR(50),
 	endereco VARCHAR(120)
-)
+);
 
 SELECT * FROM proprietario;
+
+-- Crie uma tabela chamada Imovel, com no minímo os campos:
+-- Tipo_imovel (Casa, Apto, Galpão)
+-- Valor
+-- Observacoes
+-- Detalhes
+-- Endereco
+
+CREATE TABLE imovel(
+	tipo_imovel VARCHAR(6),
+	valor MONEY,
+	observações VARCHAR(150),
+	detalhes VARCHAR(100),
+	endereço VARCHAR(120)
+);
+
+SELECT * FROM imovel;
