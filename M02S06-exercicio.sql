6d3dc8ee44769bd5f0cbec1b395dd3a84dea93ed
Pedro Alencar <pedromariutti@gmail.com>
2023-06-09 13:21:13 -0300

    Ex 03 - Criação da tabela proprietário

---
+++ M02S06-exercicio.sql
@@ -0,0 +1,11 @@
+-- Crie uma tabela chamda proprietario, com no minímo os campos:
+-- Nome
+-- Sobrenome
+-- Endereco
+CREATE TABLE proprietario (
+	nome VARCHAR(20),
+	sobrenome VARCHAR(50),
+	endereco VARCHAR(120)
+)
+
+SELECT * FROM proprietario;
